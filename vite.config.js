8ba99c5b3b7051c77172a6ce3d447003667de0c4
tekkon7278 <st6.048153@gmail.com>
2023-06-02 19:19:13 +0900

    some update

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                //'resources/css/app.css',
                 'resources/scss/base.scss',
-                'resources/scss/cms.scss',
                 'resources/js/app.js'
             ],
             refresh: true,
